76a9d4bab07bcd6020c42860587f4a9ca696108b
Igor Slobodskov <igor.slobodskov@phystech.edu>
2020-07-09 17:08:07 +0300

    add ast

---
+++ interpreter/src/main/kotlin/com/github/kright/interpreter/ast.kt
@@ -0,0 +1,22 @@
+package com.github.kright.interpreter
+
+data class Op(val symbol: Char)
+
+sealed class Expression
+data class Id(val name: String) : Expression()
+data class BinOp(val left: Expression, val op: Op, val right: Expression) : Expression()
+data class NRange(val left: Expression, val right: Expression) : Expression()
+data class FuncCall(val funcName: Id, val args: Array<Expression>) : Expression()
+data class Lambda(val args: Array<Id>, val body: Expression) : Expression()
+
+sealed class NNumber : Expression()
+data class NReal(val value: Double) : NNumber()
+data class NInt(val value: Long) : NNumber()
+
+sealed class Statement
+
+data class VarDeclaration(val varName: Id, val expression: Expression) : Statement()
+data class OutExpr(val expr: Expression) : Statement()
+data class OutString(val string: String) : Statement()
+
+class Program(val statements: Array<Statement>)
