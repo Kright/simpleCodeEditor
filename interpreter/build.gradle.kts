d1e57973b3401411dad40211cdabaf99edb146cc
Igor Slobodskov <igor.slobodskov@phystech.edu>
2020-07-09 16:42:24 +0300

    add lib for better parsing

--- interpreter/build.gradle.kts
+++ interpreter/build.gradle.kts
@@ -1,14 +1,23 @@
 plugins {
+    application
     kotlin("jvm") version "1.3.72"
 }
 
-
 repositories {
     mavenCentral()
+    maven { setUrl("https://dl.bintray.com/hotkeytlt/maven") }
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("com.github.h0tk3y.betterParse:better-parse:0.4.0")
+}
+
+version = "0.1"
+group = "com.github.kright.interpreter"
+
+application {
+    mainClassName = "com.github.kright.interpreter.ParserKt"
 }
 
 tasks {
---
+++ interpreter/src/main/kotlin/com/github/kright/interpreter/parser.kt
@@ -0,0 +1,9 @@
+package com.github.kright.interpreter
+
+class Parser {
+
+}
+
+fun main(args: Array<String>) {
+    println("Hello from parser!")
+}
